a5b53e0455cc7025855bca7b1e6d30a22ff6e8a0
David Rafe <david.rafe@gmail.com>
2018-09-19 14:17:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray(array, element) {
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift()
-  return ["foo", []]
+  return ["foo", array]
 }
 function addElementToEndOfArray(array, element) {
   [...array, "foo"]
